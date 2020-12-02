9b039c4be2fc488faec96994c05b1f71a307353d
JuiceLamber <42639609+JuiceLamber@users.noreply.github.com>
2020-12-02 15:50:08 +1300

    React Cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../App.min.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +17,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
